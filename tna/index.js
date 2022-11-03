b8b2e533f3810c0c0e108e395efddfbccc94bf9f
sangltv <sangltv@proton.me>
2022-11-03 14:48:06 +0000

    rest

--- tna/index.js
+++ tna/index.js
@@ -2,7 +2,34 @@ require("dotenv").config()
 const PORT = process.env.PORT
 const express = require('express')
 const app = express()
+const data = {
+    users: [
+        {   id: 0,
+            username: "nhatlinh",
+            password: "Pass1234",
+        },
+        {   id: 1,
+            username: "congthao",
+            password: "Pass1234",
+        },
+    ],
+    posts: [
+        {
+            id: 0,
+            text: "Blap blap",
+            user: 1 
+        },
+        {
+            id: 1,
+            text: "Wow - Squid",
+            user: 1 
+        }
+    ]
+}
 app.get("/",(req,res)=>{
-    res.send("<h1>Good</h1>")
+    res.json("Main")
+})
+app.get("/posts",(req,res)=>{
+    res.json(data.posts)
 })
 app.listen(PORT)
