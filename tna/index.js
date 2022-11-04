b590a5a4c5ecccabf816da2a1087aea6caa70bc2
sangltv <sangltv@proton.me>
2022-11-04 01:26:56 +0000

    et

--- tna/index.js
+++ tna/index.js
@@ -3,35 +3,31 @@ const PORT = process.env.PORT
 const express = require('express')
 const app = express()
 const data = {
-    users: [
-        {   id: 0,
-            username: "nhatlinh",
-            password: "Pass1234",
-        },
-        {   id: 1,
-            username: "congthao",
-            password: "Pass1234",
-        },
-    ],
     posts: [
         {
             id: 0,
             text: "Blap blap",
-            user: 1 
+            user: "sanglq"
         },
         {
             id: 1,
             text: "Wow - Squid",
-            user: 1 
+            user: "namnd"
         }
     ]
 }
+app.use(express.json()) //json req.body
+
+app.get("/", (req, res)=>{
+    console.log(`${req.body}`)
+    res.send(q.body)
+})
 
 app.get("/posts",(req,res)=>{
     res.json(data.posts)
 })
 
-app.get("/posts/:id",(req,res)=>{
+app.get("/posts/:id",(req, res)=>{
     res.json(data.posts.filter(post => +req.params.id === post.id))
 })
 
