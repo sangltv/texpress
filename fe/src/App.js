b210fa5c02d2a554e349b883329c1f0ea6238b4d
sangltv <sangltv@proton.me>
2022-11-09 13:16:15 +0000

    cors

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-import {Button,H4,P} from './libs'
-
+import {Button,H4,P,Div} from './libs'
+import axios from 'axios'
+import useSWR from 'swr'
+const fetcher = url => axios.get(url).then(r => r.data)
 function App() {
+  const {data,error} = useSWR("https://3001-sangltv-texpress-2lb3h358kn3.ws-us75.gitpod.io/posts", fetcher)
+  if (error) return <P color="red">Error:{error.message}</P>
+  if (!data) return <P>Loading...</P>
+  console.log(data)
   return (
     <div className="App">
-      <H4 color="red">GOOD</H4>
+      <H4 color="red">Users</H4>
+      {
+        data.map(item => <Div>
+          <P>{item.text}</P>
+        </Div>)
+      }
       <Button variant="contained" bg="black">Click me</Button>
       <P>Nice</P>
     </div>
