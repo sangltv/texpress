8f23adb11adc246ca3b6551085ecc0c559def516
sangltv <sangltv@proton.me>
2022-11-11 02:19:11 +0000

    mock-server

---
+++ mockapi/db.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+    return{
+        users:[{
+            id:0,
+            username:"sangltv",
+            email:"sangltv@proton.me"
+        }],
+        posts:[{
+            id:0,
+            text:"Hello"
+        }]
+    }
+}
